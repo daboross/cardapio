9a8ca59ed6c1dbb0d4612bd5577c76d205069472
Thiago <tvst@blinkblinkblink>
2010-06-07 19:19:39 -0400

    Forgot to add the plugins directory last time!

---
+++ cardapio/plugins/google.py
@@ -0,0 +1,68 @@
+import simplejson, urllib2, gio
+
+if '_' not in locals():
+	_ = lambda x: x
+
+class CardapioPlugin(CardapioPluginInterface):
+
+	author             = 'Thiago Teixeira'
+	name               = 'Google plugin'
+	description        = 'Perform quick Google searches'
+	version            = '1.0'
+
+	plugin_api_version = 1.0
+
+	search_delay_type  = 'remote search update delay'
+
+	category_name      = _('Web Results')
+	category_icon      = 'system-search'
+	hide_from_sidebar  = True
+
+
+	def __init__(self, settings, cardapio_result_handler, cardapio_error_handler):
+
+		self.query_url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=%s'
+		self.timeout = settings['remote search update delay'] * 2
+		self.search_results_limit = settings['search results limit']
+		self.cardapio_result_handler = cardapio_result_handler
+		self.cardapio_error_handler = cardapio_error_handler
+		self.search_controller = gio.Cancellable()
+
+
+	def search(self, text):
+
+		text = urllib2.quote(text)
+		self.search_controller.reset()
+
+		self.stream = gio.File(self.query_url % text)
+		self.stream.load_contents_async(self.handle_search_result, cancellable = self.search_controller)
+
+
+	def cancel(self):
+
+		if not self.search_controller.is_cancelled():
+			self.search_controller.cancel()
+
+	
+	def handle_search_result(self, gdaemonfile = None, response = None):
+
+		response = self.stream.load_contents_finish(response)[0]
+		raw_results = simplejson.loads(response)
+
+		parsed_results = [] 
+
+		if 'Error' in raw_results:
+			self.cardapio_error_handler(self, raw_results['Error'])
+			return
+		
+		for raw_result in raw_results['responseData']['results']:
+
+			item = {}
+			item['name']      = raw_result['titleNoFormatting']
+			item['tooltip']   = raw_result['url']
+			item['xdg uri']   = raw_result['url']
+			item['icon name'] = 'text-html'
+			parsed_results.append(item)
+
+		self.cardapio_result_handler(self, parsed_results)
+
---
+++ cardapio/plugins/tracker.py
@@ -0,0 +1,87 @@
+import urllib2, os
+
+if '_' not in locals():
+	_ = lambda x: x
+
+class CardapioPlugin(CardapioPluginInterface):
+
+	author             = 'Thiago Teixeira'
+	name               = 'Traker plugin'
+	description        = 'Search local files and folders indexed with Tracker'
+	version            = '1.0'
+
+	plugin_api_version = 1.0
+
+	search_delay_type  = 'local search update delay'
+
+	category_name      = _('Local Results')
+	category_icon      = 'system-search'
+	hide_from_sidebar  = True
+
+
+	def __init__(self, settings, handle_search_result, handle_search_error):
+
+		self.tracker = None
+		bus = dbus.SessionBus()
+
+		if bus.request_name('org.freedesktop.Tracker1') == dbus.bus.REQUEST_NAME_REPLY_IN_QUEUE:
+			tracker_object = bus.get_object('org.freedesktop.Tracker1', '/org/freedesktop/Tracker1/Resources')
+			self.tracker = dbus.Interface(tracker_object, 'org.freedesktop.Tracker1.Resources') 
+
+		self.search_results_limit = settings['search results limit']
+
+		self.cardapio_results_handler = handle_search_result
+		self.cardapio_error_handler = handle_search_error
+
+
+	def search(self, text):
+
+		# no .lower(), since there's no fn:lower-case in tracker (yet!)
+		#text = urllib2.quote(text).lower()
+		text = urllib2.quote(text)
+
+		self.tracker.SparqlQuery(
+			"""
+				SELECT ?uri ?mime
+				WHERE { 
+					?item a nie:InformationElement;
+						nie:url ?uri;
+						nie:mimeType ?mime;
+						tracker:available true.
+					FILTER (fn:contains(?uri, "%s"))
+					}
+				ORDER BY ASC(?uri)
+				LIMIT %d
+			""" 
+			% (text, self.search_results_limit),
+			dbus_interface='org.freedesktop.Tracker1.Resources',
+			reply_handler=self.prepare_and_handle_search_result,
+			error_handler=self.handle_search_error
+			)
+
+
+	def handle_search_error(self, error):
+
+		self.cardapio_error_handler(self, error)
+
+
+	def prepare_and_handle_search_result(self, results):
+
+		formatted_results = []	
+
+		for result in results:
+			
+			dummy, canonical_path = urllib2.splittype(result[0])
+			parent_name, child_name = os.path.split(canonical_path)
+			icon_name = result[1]
+
+			formatted_result = {}
+			formatted_result['name'] = child_name
+			formatted_result['icon name'] = icon_name
+			formatted_result['tooltip'] = canonical_path
+			formatted_result['xdg uri'] = canonical_path
+
+			formatted_results.append(formatted_result)
+
+		self.cardapio_results_handler(self, formatted_results)
+
