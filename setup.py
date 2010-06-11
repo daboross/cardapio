07e8d8ed909e0b3f2bc356a78880ac287cf56dc2
Thiago <tvst@blinkblinkblink>
2010-06-11 01:17:48 -0400

    - upped the version to 0.9.90!! almost done with the milestones for 1.0!

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ os.chdir('..')
 
 setup(
 	name         = 'Cardapio',
-	version      = '0.9.80',
+	version      = '0.9.90',
 	description  = 'A menu with search capabilities.',
 	author       = 'Thiago Teixeira',
 	author_email = 'tvst@hotmail.com',
