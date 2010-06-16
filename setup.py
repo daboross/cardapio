0af8b25df1c7b8f1bdcbe9de818882bc77406350
Thiago <tvst@blinkblinkblink>
2010-06-16 02:03:00 -0400

    - updating user translations - updating version to 0.9.104

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ os.chdir('..')
 
 setup(
 	name         = 'Cardapio',
-	version      = '0.9.103',
+	version      = '0.9.104',
 	description  = 'A menu with search capabilities.',
 	author       = 'Thiago Teixeira',
 	author_email = 'tvst@hotmail.com',
