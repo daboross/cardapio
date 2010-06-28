a8dba82180110ad0f909b965ea28eaee0d41e6f5
Thiago <tvst@blinkblinkblink>
2010-06-28 01:45:36 -0400

    - now version 0.9.117

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ os.chdir('..')
 
 setup(
 	name         = 'Cardapio',
-	version      = '0.9.116',
+	version      = '0.9.117',
 	description  = 'A menu with search capabilities.',
 	author       = 'Thiago Teixeira',
 	author_email = 'tvst@hotmail.com',
