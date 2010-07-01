6ceef6e3150dd4c18f364ee482465396f19e6204
Thiago <tvst@blinkblinkblink>
2010-07-01 13:52:09 -0400

    - new setup.py for use in Gentoo (while waiting for a possible makefile fix)

--- setup.py
+++ setup.py
@@ -1,40 +1,31 @@
 from distutils.core import setup
 import os
 
-
-# automatically build package_data by traversing folders
-
 package_data = ['cardapio.ui']
 
-os.chdir('cardapio')
+data_files = [('lib/bonobo/servers', ['src/cardapio.server'])]
 
 for root, dir, files in os.walk('locale'):
-	for file_ in files:
-		if len(file_) > 3 and file_[-3:] == '.mo':
-			package_data.append(os.path.join(root, file_))
+    for file_ in files:
+        if len(file_) > 3 and file_.endswith('.mo'):
+            data_files.append( (os.path.join('share', root), [os.path.join(root, file_)]) )
 
-for root, dir, files in os.walk('plugins'):
-	for file_ in files:
-		if len(file_) > 3 and file_[-3:] == '.py':
-			package_data.append(os.path.join(root, file_))
-
-os.chdir('..')
-
-
-# install
+for root, dir, files in os.walk('src/plugins'):
+    for file_ in files:
+        if len(file_) > 3 and file_.endswith('.py'):
+            package_data.append(os.path.join(root.replace('src' + os.path.sep, ''), file_))
 
 setup(
-	name         = 'Cardapio',
-	version      = '0.9.120',
-	description  = 'A menu with search capabilities.',
-	author       = 'Thiago Teixeira',
-	author_email = 'tvst@hotmail.com',
-	url          = 'https://www.launchpad.net/cardapio',
-	packages     = ['cardapio'],
-	package_data = {'cardapio': package_data},
-	data_files   = [
-			('/usr/local/bin', ['cardapio/cardapio']),
-			('/usr/lib/bonobo/servers', ['cardapio/cardapio.server']),
-		],
+    name         = 'Cardapio',
+    version      = '0.9.120',
+    description  = 'A menu with search capabilities.',
+    author       = 'Thiago Teixeira',
+    author_email = 'tvst@hotmail.com',
+    url          = 'https://www.launchpad.net/cardapio',
+    requires     = ['gtk', 'gtk.glade', 'gio', 'glib', 'gmenu', 'keybinder', 'gnomeapplet', 'dbus', 'dbus.service', 'dbus.mainloop.glib', 'xdg', 'gnome', 'simplejson', ],
+    package_dir  = {'cardapio': 'src'},
+    packages     = ['cardapio'],
+    package_data = {'cardapio': package_data},
+    scripts      = ['src/cardapio'],
+    data_files   = data_files,
 	)
-
