a3ea9d037cd466aadc18b88f5189e82401760e2c
Pawel Bara <>
2010-07-22 19:43:42 +0200

    minor changes to Bing plugin including better handling of errors

--- src/plugins/bing.py
+++ src/plugins/bing.py
@@ -2,12 +2,21 @@ import json
 import gio
 import urllib
 
+from glib import GError
+
 class CardapioPlugin(CardapioPluginInterface):
+	"""
+	Plugin based on Microsoft's Bing API. Basics of the API are described in
+	this document:
+	http://www.bing.com/developers/s/API%20Basics.pdf
+
+	All web requests are done in asynchronous and cancellable manner.
+	"""
 
 	# Cardapio's variables
 	author = 'Pawel Bara'
 	name = _('Bing')
-	description = _('Perform a search using Microsoft\'s Bing')
+	description = _("Perform a search using Microsoft's Bing")
 	version = '0.9b'
 
 	url = ''
@@ -18,7 +27,7 @@ class CardapioPlugin(CardapioPluginInterface):
 	search_delay_type = 'remote search update delay'
 
 	category_name = _('Bing Results')
-	category_tooltip = _('Results found with Bing')
+	category_tooltip = _('Results found using Bing')
 
 	category_icon = 'system-search'
 	fallback_icon = ''
@@ -26,22 +35,23 @@ class CardapioPlugin(CardapioPluginInterface):
 	hide_from_sidebar = True
 
 	def __init__(self, cardapio_proxy):
-		cardapio_proxy.write_to_log(self, 'initializing Bing')
+		cardapio_proxy.write_to_log(self, 'initializing Bing plugin')
 
 		self.cardapio = cardapio_proxy
 
 		self.cancellable = gio.Cancellable()
 
-		# Bing API's arguments
+		# Bing's API arguments (my AppID and a request for a web search with
+		# maximum four results)
 		self.api_base_args = {
-			'Appid': '237CBC82BB8C3F7F5F19F6A77B0D38A59E8F8C2C',
+			'Appid'    : '237CBC82BB8C3F7F5F19F6A77B0D38A59E8F8C2C',
 			'sources'  : 'web',
 			'web.count': 4
 		}
-		self.api_base_url = 'http://api.search.live.net/json.aspx?{0}'
 
-		# Bing's web base URL (for purposes of 'search more' option)
-		self.web_base_url = 'http://www.bing.com/search?q={0}'
+		# Bing's base URLs (search and search more variations)
+		self.api_base_url = 'http://api.search.live.net/json.aspx?{0}'
+		self.web_base_url = 'http://www.bing.com/search?{0}'
 
 		self.loaded = True
 
@@ -58,22 +68,33 @@ class CardapioPlugin(CardapioPluginInterface):
 		current_args['query'] = text
 		final_url = self.api_base_url.format(urllib.urlencode(current_args))
 
-		self.cardapio.write_to_log(self, 'search URL: {0}'.format(final_url))
+		self.cardapio.write_to_log(self, 'final API URL: {0}'.format(final_url))
 
 		# asynchronous and cancellable IO call
 		self.current_stream = gio.File(final_url)
 		self.current_stream.load_contents_async(self.show_search_results,
-												cancellable = self.cancellable,
-												user_data = text)
+			cancellable = self.cancellable,
+			user_data = text)
 
 	def show_search_results(self, gdaemonfile, result, text):
 		"""
-        Callback to asynchronous IO (Bing API's call).
+        Callback to asynchronous IO (Bing's API call).
 		"""
 
-		json_body = self.current_stream.load_contents_finish(result)[0]
-		response = json.loads(json_body)
+		# watch out for connection problems
+		try:
+			json_body = self.current_stream.load_contents_finish(result)[0]
 
+			# watch out for empty input
+			if len(json_body) == 0:
+				return
+
+			response = json.loads(json_body)
+		except (ValueError, GError) as ex:
+			self.cardapio.handle_search_error(self, 'error while obtaining data: {0}'.format(str(ex)))
+			return
+
+		# decode the result
 		try:
 			items = []
 
@@ -84,29 +105,33 @@ class CardapioPlugin(CardapioPluginInterface):
 				# remember them all
 				for item in response_body['Results']:
 					items.append({
-									'name'         : item['Title'],
-									'tooltip'      : item['Url'],
-									'icon name'    : 'text-html',
-									'type'         : 'xdg',
-									'command'      : item['Url'],
-									'context menu' : None
-								})
+						'name'         : item['Title'],
+						'tooltip'      : item['Url'],
+						'icon name'    : 'text-html',
+						'type'         : 'xdg',
+						'command'      : item['Url'],
+						'context menu' : None
+					})
 
 			# always add 'Search more...' item
+			search_more_args = { 'q' : text }
+
 			items.append({
-							'name'         : _('Show additional results'),
-							'tooltip'      : _('Show additional search results in your web browser'),
-							'icon name'    : 'system-search',
-							'type'         : 'xdg',
-							'command'      : self.web_base_url.format(text),
-							'context menu' : None
-						})
+				'name'         : _('Show additional results'),
+				'tooltip'      : _('Show additional search results in your web browser'),
+				'icon name'    : 'system-search',
+				'type'         : 'xdg',
+				# TODO: cardapio later unquotes this and then quotes this again;
+				# it's screwing my quotation
+				'command'      : self.web_base_url.format(urllib.urlencode(search_more_args)),
+				'context menu' : None
+			})
 
 			# pass the results to Cardapio
 			self.cardapio.handle_search_result(self, items)
 
 		except KeyError:
-			self.cardapio.handle_search_error(self, 'Incorrect Bing\'s JSON structure')
+			self.cardapio.handle_search_error(self, "Incorrect Bing's JSON structure")
 
 	def cancel(self):
 		self.cardapio.write_to_log(self, 'cancelling a recent Bing search (if any)')
