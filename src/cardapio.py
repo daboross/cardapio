e92c952891fb5dbda0434654d86f4acd93edc65a
Thiago <tvst@blinkblinkblink>
2010-08-20 04:49:46 -0400

    - v0.9.138

--- src/cardapio.py
+++ src/cardapio.py
@@ -135,7 +135,7 @@ class Cardapio(dbus.service.Object):
 	bus_name_str = 'org.varal.Cardapio'
 	bus_obj_str  = '/org/varal/Cardapio'
 
-	version = '0.9.137'
+	version = '0.9.138'
 
 	core_plugins = [
 			'applications',
