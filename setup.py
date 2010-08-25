af8e9be5442feca605c3d3baea4e716a76c9a3fd
Thiago <tvst@blinkblinkblink>
2010-08-25 17:49:02 -0400

    - upped version 0.9.143

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ for root, dir, files in os.walk('src/plugins'):
 
 setup(
     name         = 'Cardapio',
-    version      = '0.9.142',
+    version      = '0.9.143',
     description  = 'A menu with search capabilities.',
     author       = 'Thiago Teixeira',
     author_email = 'tvst@hotmail.com',
