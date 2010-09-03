f35b913d750dab8d8d46dc1fb5d5ccf49c757cb5
Thiago <tvst@blinkblinkblink>
2010-09-03 16:24:55 -0400

    - upped version to 0.9.144 - added tooltip "drag to reorder plugins" to preferences window

--- src/cardapio.py
+++ src/cardapio.py
@@ -130,7 +130,7 @@ class Cardapio(dbus.service.Object):
 	bus_name_str = 'org.varal.Cardapio'
 	bus_obj_str  = '/org/varal/Cardapio'
 
-	version = '0.9.143'
+	version = '0.9.144'
 
 	core_plugins = [
 			'applications',
