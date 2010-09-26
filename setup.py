52f63bcc24efc0bf0c0bc8224b360f747e9f0711
Thiago <tvst@blinkblinkblink>
2010-09-26 23:44:21 +0200

    - eh, I had changed the version to 0.0.147 instead of 0.9.147. Fixed now.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ for root, dir, files in os.walk('src/plugins'):
 
 setup(
     name         = 'Cardapio',
-    version      = '0.0.147',
+    version      = '0.9.147',
     description  = 'A menu with search capabilities.',
     author       = 'Thiago Teixeira',
     author_email = 'tvst@hotmail.com',
