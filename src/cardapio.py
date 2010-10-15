ff2747ab7fb0fc944d576364734410a81a24fcc6
Thiago Teixeira <tvst@hotmail.com>
2010-10-15 10:19:48 +0200

    - fixed a bug introduced last night

--- src/cardapio.py
+++ src/cardapio.py
@@ -3048,8 +3048,9 @@ class Cardapio(dbus.service.Object):
 
 		# this is necessary when clearing section contents to avoid a memory
 		# leak, but does nothing when clearing other containers:
-		self.app_list = [app for app in self.app_list if app['section'] != container.parent.parent]
-		self.sys_list = [app for app in self.sys_list if app['section'] != container.parent.parent]
+		if container is not None and container.parent is not None and container.parent.parent is not None:
+			self.app_list = [app for app in self.app_list if app['section'] != container.parent.parent]
+			self.sys_list = [app for app in self.sys_list if app['section'] != container.parent.parent]
 
 		for	child in container.get_children():
 			container.remove(child)
