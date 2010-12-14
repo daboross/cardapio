9406b9f6b597a1a13f8b37aabf1a3443f19f7110
Thiago Teixeira <tvst@hotmail.com>
2010-12-14 09:39:38 +0100

    - packaged cardapio-awn - upped version to 0.9.162 and pushed to PPA

--- cardapio/src/Cardapio.py
+++ cardapio/src/Cardapio.py
@@ -132,7 +132,7 @@ class Cardapio(dbus.service.Object):
 	bus_name_str = 'org.varal.Cardapio'
 	bus_obj_str  = '/org/varal/Cardapio'
 
-	version = '0.9.161'
+	version = '0.9.162'
 
 	core_plugins = [
 			'applications',
