6549b2d39abfa694d6a3af83696e6b9178d34608
Thiago Teixeira <tvst@hotmail.com>
2010-12-15 15:51:47 +0100

    - fixed copyright/license statement

--- cardapio/src/docky/cardapio_helper.py
+++ cardapio/src/docky/cardapio_helper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #  
-#  Copyright (C) 2010 Thiago Teixeira
+#  Copyright (C) 2010 Cardapio Team (tvst@hotmail.com)
 # 
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
