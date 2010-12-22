b1afd9f109e4aacd8739f37e5ee2788747082a3a
Thiago Teixeira <tvst@hotmail.com>
2010-12-22 12:07:38 +0100

    - upped version - added a python scripts for ppa statistics!

---
+++ get_ppa_stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+from launchpadlib.launchpad import Launchpad
+PPAOWNER = "cardapio-team" # the launchpad PPA owener. It's usually the first part of a PPA. Example: in "webupd8team/vlmc", the owner is "webupd8team".
+PPA = "unstable"           # the PPA to get stats for. It's the second part of a PPA. Example: in "webupd8team/vlmc", the PPA is "vlmc"
+
+dists = ['maverick', 'lucid']
+archs = ['i386', 'amd64']
+
+url = r'https://api.edge.launchpad.net/devel/ubuntu/%s/%s'
+
+
+cachedir = "~/.launchpadlib/cache/"
+
+lp_ = Launchpad.login_anonymously('ppastats', 'edge', cachedir, version='devel')
+owner = lp_.people[PPAOWNER]
+archive = owner.getPPAByName(name=PPA)
+
+
+for dist in dists:
+	for arch in archs:
+		desired_dist_and_arch = url % (dist, arch)
+		print '\nDist: %s, Arch: %s' % (dist, arch)
+
+		for individualarchive in archive.getPublishedBinaries(status='Published', distro_arch_series=desired_dist_and_arch):
+			print ' * % -20s % -30s % 6s' % (individualarchive.binary_package_name, individualarchive.binary_package_version, str(individualarchive.getDownloadCount()))
