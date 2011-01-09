36c397a6cc4b8c65a30e4b00f7fb036e10f0758e
Thiago Teixeira <tvst@hotmail.com>
2011-01-09 23:22:08 +0100

    - oops, forgot to add a new file last time

---
+++ cardapio/src/CardapioViewInterface.py
@@ -0,0 +1,17 @@
+
+class CardapioViewInterface:
+
+	APP_BUTTON      = 0
+	CATEGORY_BUTTON = 1
+	SESSION_BUTTON  = 2
+	SIDEPANE_BUTTON = 3
+
+	PIN_MENUITEM               = 0
+	UNPIN_MENUITEM             = 1
+	ADD_SIDE_PANE_MENUITEM     = 2
+	REMOVE_SIDE_PANE_MENUITEM  = 3
+	OPEN_FOLDER_MENUITEM       = 4
+	PEEK_INSIDE_MENUITEM       = 5
+	EJECT_MENUITEM             = 6
+
+	# TODO: Add virtual methods here, once they are well-defined
