be316330c5954f23e291d39d408f0070762028fa
Thiago Teixeira <tvst@hotmail.com>
2011-05-15 19:30:32 +0200

    - Upped to version 0.9.178

--- cardapio/src/Cardapio.py
+++ cardapio/src/Cardapio.py
@@ -128,7 +128,7 @@ class Cardapio(dbus.service.Object):
 	bus_name_str = 'org.varal.Cardapio'
 	bus_obj_str  = '/org/varal/Cardapio'
 
-	version = '0.9.177'
+	version = '0.9.178'
 
 	core_plugins = [
 			'applications',
