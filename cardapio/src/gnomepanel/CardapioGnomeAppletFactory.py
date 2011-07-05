eb6e6f7c59a99f81fe3be5d97e37c3b99715370a
Thiago Teixeira <tvst@hotmail.com>
2011-07-05 12:21:29 +0200

    - Fixed bug in Gnome Panel that was causing Cardapio to show as a dot   (this was introduced a couple of commits ago)

--- cardapio/src/gnomepanel/CardapioGnomeAppletFactory.py
+++ cardapio/src/gnomepanel/CardapioGnomeAppletFactory.py
@@ -17,9 +17,10 @@
 
 from Cardapio import Cardapio
 from CardapioGnomeApplet import CardapioGnomeApplet
+import Constants 
 
 def CardapioGnomeAppletFactory(applet, iid):
 
 	panel_applet = CardapioGnomeApplet(applet)
-	cardapio = Cardapio(show = Cardapio.DONT_SHOW, panel_applet = panel_applet)
+	cardapio = Cardapio(show = Constants.DONT_SHOW, panel_applet = panel_applet)
 
